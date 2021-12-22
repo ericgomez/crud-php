6489d0a095d3d8402a4b3265f320392ca2d84822
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-12-22 15:40:45 -0600

    Add view listed and view create

--- crud/views/employees/create.php
+++ crud/views/employees/create.php
@@ -1,1 +1,20 @@
-Form create
+<div class="card">
+  <div class="card-header">
+    Add Employee
+  </div>
+  <div class="card-body">
+    <form action="" method="post">
+      <div class="mb-2">
+        <label for="inputName">Name:</label>
+        <input type="text" class="form-control" id="inputName" name="inputName" aria-describedby="nameHelp" placeholder="Enter name">
+      </div>
+
+      <div class="mb-2">
+        <label for="inputEmail1">Email:</label>
+        <input type="email" class="form-control" id="inputEmail1" name="inputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      </div>
+    
+      <button type="submit" class="btn btn-success">Create employee</button>
+    </form>
+  </div>
+</div>
--- crud/views/employees/initiation.php
+++ crud/views/employees/initiation.php
@@ -1,1 +1,18 @@
-Form initiation
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Eric</td>
+      <td>eric@gmail.com</td>
+      <td>Edit | Delete</td>
+    </tr>
+  </tbody>
+</table>
