3913d8e5cad8de351f7683187f392620fc0ed059
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-12-22 13:58:33 -0600

    Add router

---
+++ crud/controllers/pages_controller.php
@@ -0,0 +1,14 @@
+<?php
+
+class PagesController {
+  public function initiation() {
+    include_once('views/pages/initiation.php');
+  }
+
+
+  public function error() {
+    include_once('views/pages/error.php');
+  }
+}
+
+?>
---
+++ router.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once("controllers/pages_controller.php");
+
+
+$controller = new PagesController();
+$controller->initiation();
+
+
+
+?>
