7970f41c9b771a58041c08b771e5cf893fc1fff8
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-12-23 13:04:09 -0600

    Create connection of database

--- crud-ajax-jquery/index.php
+++ crud-ajax-jquery/index.php
@@ -1,5 +1,17 @@
 <?php
 
+$host="db";
+$db="app_db";
+$user="root";
+$pass="root";
 
+
+try {
+  $connection = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+  echo "Connected to $db at $host successfully.";
+
+} catch(Exception $e) {
+  echo $e->getMessage();
+}
 
 ?>
